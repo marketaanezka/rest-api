4acee14dd5a3e8264695cdbaea5a88b74377fa5e
Marketa Willis <marketa.willis@applifting.cz>
2024-08-17 18:56:06 +0200

    fix file name

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 
 app.use(express.json());
